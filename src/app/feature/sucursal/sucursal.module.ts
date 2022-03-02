0023d7c6557455946e2c69e556bddab494c45d18
lromero <lromero@fractalservices.com.pe>
2022-03-02 12:40:35 -0500

    add changes

--- src/app/feature/sucursal/sucursal.module.ts
+++ src/app/feature/sucursal/sucursal.module.ts
@@ -7,6 +7,7 @@ import { SucursalRoutingModule } from './sucursal-routing.module';
 
 import { RegistrarSucursalComponent } from './components/registrar-sucursal/registrar-sucursal.component';
 import { DatePipe } from '@angular/common';
+import { MaterialModule } from '@core/material/material.module';
 
 
 @NgModule({
@@ -18,6 +19,7 @@ import { DatePipe } from '@angular/common';
   imports: [
     SharedModule,
     SucursalRoutingModule,
+    MaterialModule
   ],
   providers: [SucursalService, DatePipe],
 })
